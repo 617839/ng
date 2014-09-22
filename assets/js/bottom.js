b3e3fad80e39deb6571f520f9bc6d65242f6a17d
Julienedies <julienedies@gamil.com>
2014-09-22 10:02:39 +0800

    add location list

--- assets/js/bottom.js
+++ assets/js/bottom.js
@@ -5,7 +5,7 @@
 
 (function () {
 
-    var dbApp = angular.module('dbApp', ['ngRoute', 'ctrls', 'servs']);
+    var dbApp = angular.module('dbApp', ['ctrls', 'servs']);
 
     var ctrls = angular.module('ctrls', []);
 
@@ -38,6 +38,22 @@
         $scope.upList = r.upMarginRef;
         $scope.prevList = r.downMarginRef;
         $scope.nextList = r.next;
+
+
+        //计算位置列表
+        var locaList = [];
+        for (var arr1, arr2, result, i = 1, len = ng.length; i < len; i++) {
+
+            arr1 = ng[i - 1].red;
+            arr2 = ng[i].red;
+            result = locaCompare(arr1, arr2);
+
+            locaList.push(result);
+        }
+
+        $scope.locaList = locaList;
+
+        console.log(locaList);
 
     });
 
