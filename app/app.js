53cfc6ac60ea79200cfa70dc2b19007f5a706e67
Julien <julienedies@gmail.com>
2015-05-27 17:47:00 +0800

    refactor

--- app/app.js
+++ app/app.js
@@ -3,11 +3,17 @@
  */
 
 var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var sto = require('../data/sto.js');
+
+//全局变量定义
+global.ROOT_DIR = path.join(__dirname, '../');
+global.HELP_DIR = path.join(__dirname, './helper/');
+global.DATA_DIR = path.join(ROOT_DIR, './data/');
+
 
 //设置静态文件服务器目录
 app.use(express.static(__dirname + '/../'));
@@ -33,34 +39,42 @@ app.use( function(req, res, next) {
 });
 
 //bodyParser组件
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-//
-app.post('/lottery', function(req, res){
+//数据收集接口
+app.post('/gather/:type', function(req, res){
+
+    var lotteryParser = require(HELP_DIR + 'lottery-parser.js');
 
     var data = req.body;
 
-    fs.writeFileSync('../data/data.json', JSON.stringify(data));
+    var type = req.params.type;
 
-    fs.writeFileSync('33.js', 'window.NGGLOBAL = window.NGGLOBAL || {};');
+    var jsonPath = path.join(DATA_DIR, type + '/data.json');
 
-    var obj = sto.resolve(data);
+    var jsPath = path.join(DATA_DIR, type + '/data.js');
+
+    fs.writeFileSync(jsonPath, JSON.stringify(data));
+    fs.writeFileSync(jsPath, 'window.NGGLOBAL = window.NGGLOBAL || {};');
+    
+    var dob = lotteryParser.resolve(data);
 
     var t;
 
-    for (var i in obj) {
+    for (var i in dob) {
 
-        t = '\r\nNGGLOBAL.' + i + ' = window.' + i + ' = ' + JSON.stringify(obj[i]) + ';';
+        t = '\r\n' + 'NGGLOBAL.' + i + ' = window.' + i + ' = ' + JSON.stringify(dob[i]) + ';';
 
-        fs.appendFile('33.js', t, function(err) {
+        fs.appendFile(jsPath, t, function(err) {
+
             err && console.log(err);
+
         });
 
     }
 
     res.send('It\'s ok. To complete the receiving.');
-    console.log(data);
 
 });
 
@@ -105,5 +119,5 @@ app.del('/status/:type?', function(reg, res){
 
 
 //启动server
-app.listen(2014);
-console.log('server start on port 2014.');
+app.listen(2017);
+console.log('server start on port 2017.');
